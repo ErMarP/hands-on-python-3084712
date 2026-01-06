eae2504e301b2ffef799f7193e9c873d372e2caf
ErMarP <53274732+ErMarP@users.noreply.github.com>
2026-01-06 00:43:13 +0000

    End of the course

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -1,18 +1,19 @@
 NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
+Pares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
 JOHN_PAUL = NAMES[:2]
 GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
-EVERY_OTHER = NAMES[::2]
+REVERSE = Pares[::-1]
+EVERY_OTHER = Pares[3::2]
 
 print(sum(AGES))
 print(min(AGES))
 print(max(AGES))
 
 print(JOHN_PAUL)
-print(GEORGE_RINGO)
+print(EVERY_OTHER)
 print(REVERSE)
